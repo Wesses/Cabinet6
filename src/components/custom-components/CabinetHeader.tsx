b6829a11481f4884ed24d057b0b8d686494eaaea
Anton Tsurkan <wesses1999@gmail.com>
2025-04-27 02:05:29 +0300

    add button back to cabinet

--- src/components/custom-components/CabinetHeader.tsx
+++ src/components/custom-components/CabinetHeader.tsx
@@ -2,18 +2,15 @@ import { useNavigate } from "react-router";
 import { Button } from "@/components/ui/button";
 import Cookies from "js-cookie";
 import { useContext } from "react";
-import { CURRENT_PAGE_PARAM_KEY, localStorages } from "@/utils/constants";
+import { localStorages } from "@/utils/constants";
 import { UserContext } from "@/contexts/UserContext";
 import LocaleButton from "./LocaleButton";
 import { useTranslation } from "react-i18next";
-import { useSearchParams } from 'react-router-dom';
-import { cn } from '@/lib/utils';
 
 export const CabinetHeader = () => {
   const navigate = useNavigate();
   const { username, companyName } = useContext(UserContext);
   const { t } = useTranslation();
-  const [SearchParams] = useSearchParams();
   
   const handleLogout = () => {
     Cookies.remove(import.meta.env.VITE_TOKEN_NAME, {
@@ -22,12 +19,6 @@ export const CabinetHeader = () => {
     navigate("/login");
     localStorage.removeItem(localStorages.USER_DATA);
   };
-
-  const hadnleBackToCabinet = () => {
-    navigate(`/cabinet?${CURRENT_PAGE_PARAM_KEY}=1`);
-  }
-
-  const isCabinetPage = SearchParams.get(CURRENT_PAGE_PARAM_KEY);
 
   return (
     <header className="sticky top-0 w-full bg-zinc-900 py-6 px-6 flex sm:flex-row sm:justify-between sm:gap-0 sm:items-center shadow-lg z-10 flex-col gap-2 justify-center max-h-[88px]">
@@ -41,21 +32,13 @@ export const CabinetHeader = () => {
       <div className="flex flex-row items-center sm:justify-normal justify-between gap-x-4">
         <LocaleButton isLabel={false} />
 
-        <div className='flex flex-col gap-y-2'>
           <Button
-            className={cn({"h-6": !isCabinetPage}, "bg-white text-zinc-900 hover:bg-gray-300 transition-all duration-300 rounded-md" )}
+            className="bg-white text-zinc-900 hover:bg-gray-300 transition-all duration-300 rounded-md"
             onClick={handleLogout}
           >
             {t("button_left_cabinet")}
           </Button>
 
-          <Button
-            className={cn({"hidden": isCabinetPage}, "bg-white text-zinc-900 hover:bg-gray-300 transition-all duration-300 rounded-md h-6" )}
-            onClick={hadnleBackToCabinet}
-          >
-            {t("back_to_cabinet_page")}
-          </Button>
-        </div>
       </div>
     </header>
   );
--- src/pages/InvoicePage.tsx
+++ src/pages/InvoicePage.tsx
@@ -3,7 +3,7 @@ import { Skeleton } from "@/components/ui/skeleton";
 import { Fragment, useEffect, useMemo, useState } from "react";
 import { getAbonentCardT, WaterSupplyDataT, TabsNamesT } from "@/types";
 import { getAbonentCardData } from "@/api/api";
-import { useParams } from "react-router";
+import { useNavigate, useParams } from "react-router";
 import { Button } from "@/components/ui/button";
 import InvoiceDataTab from "@/components/custom-components/InvoiceServicesTabs/InvoiceDataTab";
 import WaterSupplyTab from "@/components/custom-components/InvoiceServicesTabs/WaterSupplyTab";
@@ -11,6 +11,9 @@ import { onMainPage } from "@/utils/onMainPage";
 import WaterSupplyAbplPodachaTab from "@/components/custom-components/InvoiceServicesTabs/WaterSupplyAbplPodachaTab";
 import WaterSupplyAbplStokiTab from "@/components/custom-components/InvoiceServicesTabs/WaterSupplyAbplStokiTab";
 import { useSearchParams } from "react-router-dom";
+import { CURRENT_PAGE_PARAM_KEY } from "@/utils/constants";
+import { ArrowLeftToLine } from "lucide-react";
+import { cn } from '@/lib/utils';
 
 const SEARCH_PARAM_TAB_KEY = "tab";
 
@@ -22,10 +25,7 @@ const getWaterSupplyData = (rowData: WaterSupplyDataT | undefined) => {
 
   if (rowData.vodaPodacha) {
     cookedData.push(
-      [
-        "Кількість побутових водомірів",
-        rowData.vodaAbSchetchikiKolvo,
-      ],
+      ["Кількість побутових водомірів", rowData.vodaAbSchetchikiKolvo],
       ["Тариф на подачу води (грн./м³)", rowData.tsenaPodacha]
     );
   }
@@ -52,6 +52,7 @@ const CabinetPage = () => {
   const [isError, setIsError] = useState(false);
   const { id } = useParams();
   const [searchParams, setSearchParams] = useSearchParams();
+  const navigate = useNavigate();
 
   useEffect(() => {
     setIsError(false);
@@ -142,54 +143,77 @@ const CabinetPage = () => {
     [abonentCardData, isWaterSupply, isWaterSupplyFee, isWaterSupplyDrainage]
   );
 
+  const hadnleBackToCabinet = () => {
+    navigate(`/cabinet?${CURRENT_PAGE_PARAM_KEY}=1`);
+  };
+
+  const isContent = !isLoading && !isError;
+
   return (
-    <div className="px-5 py-3 flex-1 h-full w-full">
+    <div className="px-5 py-2 flex-1 h-full w-full">
       {isLoading && (
         <div className="w-full h-full">
+          <Skeleton className="w-1/6 h-8 min-w-[200px] mb-2 bg-slate-300" />
           <Skeleton className="md:w-1/2 w-full h-[40px] mb-2 bg-slate-300" />
           <Skeleton className="md:w-1/2 w-full h-5/6 bg-slate-300" />
         </div>
       )}
 
-      {isError && (
-        <div className="flex flex-col gap-1 justify-center items-center w-full h-full">
-          <h1>Сталася попилка оновіть сторінку</h1>
-          <div className="flex flex-row gap-2">
-            <Button onClick={() => window.location.reload()}>Оновити</Button>
-            <Button onClick={onMainPage}>На головну</Button>
-          </div>
-        </div>
-      )}
-
-      {!isLoading && !isError && (
-        <Tabs
-          className="md:w-1/2 w-full md:min-w-[700px] h-full"
-          value={searchParams.get(SEARCH_PARAM_TAB_KEY) || TabsNamesT.Invoice_data}
+      <div className={cn({"md:w-1/2 md:min-w-[700px]": isContent,
+        "items-center h-full": isError,
+      }, "flex flex-col gap-y-1 w-full")}>
+        <Button
+          className="bg-gray-300 hover:bg-gray-500  text-zinc-900 transition-all duration-300 rounded-md h-8 w-1/3 min-w-[200px]"
+          onClick={hadnleBackToCabinet}
         >
-          <TabsList className="w-full flex justify-start sticky sm:top-24 top-32 z-10 overflow-x-auto whitespace-nowrap">
-            {tabListParams.map(({ value, label, condition }) => (
+          <div className="flex w-auto justify-center items-center gap-x-2">
+            <ArrowLeftToLine />
+            <p>Повернутися до списку</p>
+          </div>
+        </Button>
+
+        {isError && (
+          <div className="flex flex-col gap-1 justify-center items-center w-full h-full">
+            <h1>Сталася попилка оновіть сторінку</h1>
+            <div className="flex flex-row gap-2">
+              <Button onClick={() => window.location.reload()}>Оновити</Button>
+              <Button onClick={onMainPage}>На головну</Button>
+            </div>
+          </div>
+        )}
+
+        {isContent && (
+          <Tabs
+            className="w-full md:min-w-[700px] h-full"
+            value={
+              searchParams.get(SEARCH_PARAM_TAB_KEY) || TabsNamesT.Invoice_data
+            }
+          >
+            <TabsList className="w-full flex justify-start sticky sm:top-24 top-32 z-10 overflow-x-auto whitespace-nowrap">
+              {tabListParams.map(({ value, label, condition }) => (
+                <Fragment key={value}>
+                  {condition && (
+                    <TabsTrigger
+                      value={value}
+                      onClick={() => handlSetSearchParams(value)}
+                    >
+                      {label}
+                    </TabsTrigger>
+                  )}
+                </Fragment>
+              ))}
+            </TabsList>
+
+            {tabListParams.map(({ value, condition, tab_component }) => (
               <Fragment key={value}>
                 {condition && (
-                  <TabsTrigger
-                    value={value}
-                    onClick={() => handlSetSearchParams(value)}
-                  >
-                    {label}
-                  </TabsTrigger>
+                  <TabsContent value={value}>{tab_component}</TabsContent>
                 )}
               </Fragment>
             ))}
-          </TabsList>
-
-          {tabListParams.map(({ value, condition, tab_component }) => (
-            <Fragment key={value}>
-              {condition && (
-                <TabsContent value={value}>{tab_component}</TabsContent>
-              )}
-            </Fragment>
-          ))}
-        </Tabs>
-      )}
+          </Tabs>
+        )}
+      </div>
     </div>
   );
 };
