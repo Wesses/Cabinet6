31d0d2e1cc87dd755e02a663ad2b4cb1a73cd443
Anton Tsurkan <wesses1999@gmail.com>
2025-04-27 03:20:17 +0300

    refactor

--- src/pages/InvoicePage.tsx
+++ src/pages/InvoicePage.tsx
@@ -1,6 +1,6 @@
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 import { Skeleton } from "@/components/ui/skeleton";
-import { Fragment, useEffect, useMemo, useState } from "react";
+import { useEffect, useMemo, useState } from "react";
 import { getAbonentCardT, TabsNamesT } from "@/types";
 import { getAbonentCardData } from "@/api/api";
 import { useNavigate, useParams } from "react-router";
@@ -13,7 +13,7 @@ import WaterSupplyAbplStokiTab from "@/components/custom-components/InvoiceServi
 import { useSearchParams } from "react-router-dom";
 import { CURRENT_PAGE_PARAM_KEY } from "@/utils/constants";
 import { ArrowLeftToLine } from "lucide-react";
-import { cn } from '@/lib/utils';
+import { cn } from "@/lib/utils";
 
 const SEARCH_PARAM_TAB_KEY = "tab";
 
@@ -28,7 +28,7 @@ const CabinetPage = () => {
 
   useEffect(() => {
     setIsError(false);
-    if (!searchParams.get(SEARCH_PARAM_TAB_KEY)) {
+    if (!searchParams.has(SEARCH_PARAM_TAB_KEY)) {
       setSearchParams({ [SEARCH_PARAM_TAB_KEY]: TabsNamesT.Invoice_data });
     }
 
@@ -80,9 +80,7 @@ const CabinetPage = () => {
         label: "Водопостачання",
         condition: isWaterSupply,
         tab_component: (
-          <WaterSupplyTab
-            waterSupplyData={abonentCardData?.voda}
-          />
+          <WaterSupplyTab waterSupplyData={abonentCardData?.voda} />
         ),
       },
 
@@ -131,9 +129,15 @@ const CabinetPage = () => {
         </div>
       )}
 
-      <div className={cn({"md:w-1/2 md:min-w-[700px]": isContent,
-        "items-center h-full": isError,
-      }, "flex flex-col gap-y-1 w-full")}>
+      <div
+        className={cn(
+          {
+            "md:w-1/2 md:min-w-[700px]": isContent,
+            "items-center h-full": isError,
+          },
+          "flex flex-col gap-y-1 w-full"
+        )}
+      >
         <Button
           className="bg-gray-300 hover:bg-gray-500  text-zinc-900 transition-all duration-300 rounded-md h-8 w-1/3 min-w-[200px]"
           onClick={hadnleBackToCabinet}
@@ -162,27 +166,26 @@ const CabinetPage = () => {
             }
           >
             <TabsList className="w-full flex justify-start sticky sm:top-24 top-32 z-10 overflow-x-auto whitespace-nowrap">
-              {tabListParams.map(({ value, label, condition }) => (
-                <Fragment key={value}>
-                  {condition && (
-                    <TabsTrigger
-                      value={value}
-                      onClick={() => handlSetSearchParams(value)}
-                    >
-                      {label}
-                    </TabsTrigger>
-                  )}
-                </Fragment>
-              ))}
+              {tabListParams.map(({ value, label, condition }) =>
+                condition ? (
+                  <TabsTrigger
+                    key={value}
+                    value={value}
+                    onClick={() => handlSetSearchParams(value)}
+                  >
+                    {label}
+                  </TabsTrigger>
+                ) : null
+              )}
             </TabsList>
 
-            {tabListParams.map(({ value, condition, tab_component }) => (
-              <Fragment key={value}>
-                {condition && (
-                  <TabsContent value={value}>{tab_component}</TabsContent>
-                )}
-              </Fragment>
-            ))}
+            {tabListParams.map(({ value, condition, tab_component }) =>
+              condition ? (
+                <TabsContent key={value} value={value}>
+                  {tab_component}
+                </TabsContent>
+              ) : null
+            )}
           </Tabs>
         )}
       </div>
