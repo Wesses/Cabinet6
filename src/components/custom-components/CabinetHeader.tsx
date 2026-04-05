3799ff6d92cde5295c33e76d86267dd904b46bfd
Wesses <wesses1999@gmail.com>
2026-04-05 22:24:19 +0300

    add logo

--- src/components/custom-components/CabinetHeader.tsx
+++ src/components/custom-components/CabinetHeader.tsx
@@ -2,7 +2,7 @@ import { useNavigate } from "react-router";
 import { Button } from "@/components/ui/button";
 import Cookies from "js-cookie";
 import { useContext } from "react";
-import { localStorages } from "@/utils/constants";
+import { izmteploTag, localStorages } from "@/utils/constants";
 import { UserContext } from "@/contexts/UserContext";
 import LocaleButton from "./LocaleButton";
 import { useTranslation } from "react-i18next";
@@ -11,7 +11,7 @@ export const CabinetHeader = () => {
   const navigate = useNavigate();
   const { username, companyName } = useContext(UserContext);
   const { t } = useTranslation();
-  
+
   const handleLogout = () => {
     Cookies.remove(import.meta.env.VITE_TOKEN_NAME, {
       path: import.meta.env.VITE_BASE_URL,
@@ -21,24 +21,33 @@ export const CabinetHeader = () => {
   };
 
   return (
-    <header className="sticky top-0 w-full bg-zinc-900 py-6 px-6 flex sm:flex-row sm:justify-between sm:gap-0 sm:items-center shadow-lg z-10 flex-col gap-2 justify-center max-h-[88px]">
-      <div className="flex sm:items-center gap-4 sm:justify-normal justify-between">
-        <p className="text-white md:text-xl text-base font-bold">
+    <header className="sticky top-0 w-full bg-zinc-900 py-6 px-6 flex sm:flex-row sm:justify-between sm:gap-0 sm:items-center shadow-lg z-10 flex-col gap-2 justify-center max-h-[100px]">
+      <div className="flex items-center justify-between gap-4 ite sm:items-center sm:justify-normal">
+        {import.meta.env.VITE_ALIAS === izmteploTag && (
+          <div className="rounded-full bg-slate-700 size-10 sm:size-20">
+            <img
+              src={`${import.meta.env.VITE_BASE_URL}/izmteplo_logo.png`}
+              alt="logo"
+              className="size-10 sm:size-20"
+            />
+          </div>
+        )}
+
+        <p className="text-base font-bold text-white md:text-xl">
           {companyName}
         </p>
-        <p className="text-gray-500 md:text-base text-sm">({username})</p>
+        <p className="text-sm text-gray-500 md:text-base">({username})</p>
       </div>
 
-      <div className="flex flex-row items-center sm:justify-normal justify-between gap-x-4">
+      <div className="flex flex-row items-center justify-between sm:justify-normal gap-x-4">
         <LocaleButton isLabel={false} />
 
-          <Button
-            className="bg-white text-zinc-900 hover:bg-gray-300 transition-all duration-300 rounded-md"
-            onClick={handleLogout}
-          >
-            {t("button_left_cabinet")}
-          </Button>
-
+        <Button
+          className="transition-all duration-300 bg-white rounded-md text-zinc-900 hover:bg-gray-300"
+          onClick={handleLogout}
+        >
+          {t("button_left_cabinet")}
+        </Button>
       </div>
     </header>
   );
